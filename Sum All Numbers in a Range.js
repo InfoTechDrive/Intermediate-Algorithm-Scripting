8d5752a3e0a3611e5e1b837db734963053cc19cf
adeinfo <adeinfo2015@gmail.com>
2020-05-10 16:46:12 +0100

    updates

--- Sum All Numbers in a Range.js
+++ Sum All Numbers in a Range.js
@@ -13,3 +13,32 @@ function sumAll(arr) {
   
   sumAll([1, 4]);
   sumAll([5, 10]);
+
+//   FCC Solution 1
+  function sumAll(arr) {
+    var max = Math.max(arr[0], arr[1]);
+    var min = Math.min(arr[0], arr[1]);
+    var temp = 0;
+    for (var i = min; i <= max; i++) {
+      temp += i;
+    }
+    return temp;
+  }
+  
+  sumAll([1, 4]);
+
+  
+//   FCC Solution II
+const sumAll = arr => {
+    // Buckle up everything to one!
+    const startNum = arr[0];
+    const endNum = arr[1];
+  
+    // Get the count of numbers between the two numbers by subtracting them and add 1 to the absolute value.
+    // ex. There are |1-4| + 1 = 4, (1, 2, 3, 4), 4 numbers between 1 and 4.
+    const numCount = Math.abs(startNum - endNum) + 1;
+  
+    // Using Arithmetic Progression summing formula
+    const sum = ((startNum + endNum) * numCount) / 2;
+    return sum;
+  };
